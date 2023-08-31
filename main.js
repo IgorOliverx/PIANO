c52fd3f6fef74b8c84e712344ee2a6761b8db2cc
IgorOliverX <igorferoli019@gmail.com>
2023-08-31 11:34:21 -0300

    first commit

---
+++ main.js
@@ -0,0 +1,126 @@
+const btn = document.getElementById('btn');
+btn.addEventListener('click', function () {
+    const audio = document.getElementById('audio');
+    audio.play();
+    
+    //tentei fazer com que ondas sonoras aparecerem ao teclar, funcionou, mas nao esta bem executado
+
+
+})
+
+function mostrarOnda () {
+    const vid = document.getElementById('vid');
+    vid.style.display = "block";
+}
+
+const btn1 = document.getElementById('btn1');
+btn1.addEventListener('click', function () {
+    const audio1 = document.getElementById('audio1');
+    audio1.play();
+})
+
+const btn2 = document.getElementById('btn2');
+btn2.addEventListener('click', function() {
+    const audio2 = document.getElementById('audio2');
+    audio2.play();
+})
+
+const btn3 = document.getElementById('btn3');
+btn3.addEventListener('click', function() {
+    const audio2 = document.getElementById('audio3');
+    audio3.play();
+})
+
+const btn4 = document.getElementById('btn4');
+btn4.addEventListener('click', function() {
+    const audio4 = document.getElementById('audio4');
+    audio4.play();
+})
+const btn5 = document.getElementById('btn5');
+btn5.addEventListener('click', function() {
+    const audio5 = document.getElementById('audio5');
+    audio5.play();
+})
+
+
+const btn6 = document.getElementById('btn6');
+btn6.addEventListener('click', function() {
+    const audio5 = document.getElementById('audio6');
+    audio6.play();
+})
+
+const btn7 = document.getElementById('btn7');
+btn7.addEventListener('click', function() {
+    const audio5 = document.getElementById('audio7');
+    audio7.play();
+})
+
+//repetição - deveria ter feito um laço né - nao consegui :(
+
+const btn8 = document.getElementById('btn8');
+btn8.addEventListener('click', function () {
+    const audio8 = document.getElementById('audio8');
+    audio8.play();
+})
+
+const btn9 = document.getElementById('btn9');
+btn9.addEventListener('click', function () {
+    const audio1 = document.getElementById('audio9');
+    audio9.play();
+})
+
+const btn12 = document.getElementById('btn12');
+btn12.addEventListener('click', function() {
+    const audio12 = document.getElementById('audio12');
+    audio12.play();
+})
+
+const btn13 = document.getElementById('btn13');
+btn13.addEventListener('click', function() {
+    const audio12 = document.getElementById('audio13');
+    audio13.play();
+})
+
+const btn14 = document.getElementById('btn14');
+btn14.addEventListener('click', function() {
+    const audio14 = document.getElementById('audio14');
+    audio14.play();
+})
+const btn15 = document.getElementById('btn15');
+btn5.addEventListener('click', function() {
+    const audio15 = document.getElementById('audio15');
+    audio15.play();
+})
+
+
+const btn16 = document.getElementById('btn16');
+btn16.addEventListener('click', function() {
+    const audio16 = document.getElementById('audio16');
+    audio16.play();
+})
+
+const btn17 = document.getElementById('btn17');
+btn17.addEventListener('click', function() {
+    const audio17 = document.getElementById('audio17');
+    audio17.play();
+})
+
+
+
+
+// OUTRO JEITO DE FAZER É POR MEIO DE FUNÇÕES - TESTEI ESSE INICIALMENTE COM ITERAÇÃO MAS NAO CONSEGUI O som, AGORA FUNCIONOU - LEMBRE-SE DE COLOCAR A FUNCTION NA CLASSE DO BOTAO COM O ONCLICK :)
+// function som() {
+//     btn.addEventListener('click', function() {
+//         const audio = document.getElementById('audio')
+//         audio.play();
+//     } )
+// }
+
+
+
+
+
+
+
+
+
