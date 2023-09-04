c669e7401079071d74694fe2373f85cb1441b860
IgorOliverX <igorferoli019@gmail.com>
2023-09-04 11:22:32 -0300

    ARRUMAR RESPONSIVIDADE

--- main.js
+++ main.js
@@ -100,8 +100,122 @@ btn16.addEventListener('click', function() {
 
 const btn17 = document.getElementById('btn17');
 btn17.addEventListener('click', function() {
-    const audio17 = document.getElementById('audio17');
+    const audio17 = document.getElementById('audio');
     audio17.play();
+})
+
+
+const btn18 = document.getElementById('btn18');
+btn18.addEventListener('click', function() {
+    const audio18 = document.getElementById('audio1');
+    audio17.play();
+})
+
+
+
+const btn19 = document.getElementById('btn19');
+btn19.addEventListener('click', function() {
+    const audio19 = document.getElementById('audio2');
+    audio19.play();
+})
+
+const btn20 = document.getElementById('btn20');
+btn20.addEventListener('click', function() {
+    const audio20 = document.getElementById('audio3');
+    audio20.play();
+})
+
+
+const btn21 = document.getElementById('btn21');
+btn21.addEventListener('click', function() {
+    const audio21 = document.getElementById('audio4');
+    audio21.play();
+})
+
+const btn22 = document.getElementById('btn22');
+btn22.addEventListener('click', function() {
+    const audio22 = document.getElementById('audio5');
+    audio22.play();
+})
+
+const btn23 = document.getElementById('btn23');
+btn23.addEventListener('click', function() {
+    const audio23 = document.getElementById('audio6');
+    audio23.play();
+})
+
+
+const btn24 = document.getElementById('btn24');
+btn24.addEventListener('click', function() {
+    const audio24 = document.getElementById('audio7');
+    audio24.play();
+})
+
+const btn25 = document.getElementById('btn25');
+btn25.addEventListener('click', function() {
+    const audio25 = document.getElementById('audio8');
+    audio25.play();
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+const btn26 = document.getElementById('btn26');
+btn26.addEventListener('click', function() {
+    const audio26 = document.getElementById('audio');
+    audio26.play();
+})
+
+
+const btn27 = document.getElementById('btn27');
+btn27.addEventListener('click', function() {
+    const audio27 = document.getElementById('audio1');
+    audio27.play();
+})
+
+const btn28 = document.getElementById('btn28');
+btn28.addEventListener('click', function() {
+    const audio28 = document.getElementById('audio2');
+    audio28.play();
+})
+
+const btn29 = document.getElementById('btn29');
+btn29.addEventListener('click', function() {
+    const audio29 = document.getElementById('audio3');
+    audio29.play();
+})
+
+
+const btn30 = document.getElementById('btn30');
+btn30.addEventListener('click', function() {
+    const audio30 = document.getElementById('audio4');
+    audio30.play();
+})
+
+const btn31 = document.getElementById('btn31');
+btn31.addEventListener('click', function() {
+    const audio31 = document.getElementById('audio5');
+    audio31.play();
+})
+const btn32 = document.getElementById('btn32');
+btn32.addEventListener('click', function() {
+    const audio32 = document.getElementById('audio6');
+    audio32.play();
 })
 
 
